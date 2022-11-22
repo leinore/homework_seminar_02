82f6d1e659d1f0ff995538c00d609183997928ec
leinore <116540768+leinore@users.noreply.github.com>
2022-11-22 11:54:32 +0300

    add task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Трёхзначное число {number}");
+
+int numberA = number%100;
+int numberB = numberA/10;
+
+Console.WriteLine($"Второе число это {numberB}");
